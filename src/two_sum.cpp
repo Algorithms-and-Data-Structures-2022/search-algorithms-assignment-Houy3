4b1481dd5de85bbccb6e296f0c1de800b0688648
Houy3 <ockap200304008@gmail.com>
2022-04-26 20:07:27 +0300

    .

--- src/two_sum.cpp
+++ src/two_sum.cpp
@@ -6,6 +6,21 @@ namespace assignment {
 
     // Tips: для создания пары (pair) используйте функцию std::make_pair
 
+    int left = 0;
+    int right = data.size() - 1;
+
+    while (left <= right) {
+      int curr = data[left] + data[right];
+      if (curr == sum) {
+        return std::make_pair(left, right);
+      }
+      else if (curr > sum) {
+        right--;
+      }
+      else {
+        left++;
+      }
+    }
     return std::nullopt;
   }
 
